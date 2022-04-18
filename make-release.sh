43d6f63a40af8fd34f02c09bb7ffdcf5be9e3842
Gaurav Kumar <gaurav.jgec@gmail.com>
2022-04-18 08:06:12 +0530

    Release v1.1

---
+++ make-release.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+# SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+[[ -n "${DEBUG:-}" ]] && set -x
+
+log() { echo "$1" >&2; }
+fail() { log "$1"; exit 1; }
+
+if [ $# -ne 2 ]
+then
+    fail "Need servicename and tag arguments"
+fi
+SERVICE=$1
+TAG=$2
+REPO_PREFIX="556070338223.dkr.ecr.us-east-1.amazonaws.com"
+
+
+if [[ "$TAG" != v* ]]; then
+    fail "\$TAG must start with 'v', e.g. v0.1.0 (got: $TAG)"
+fi
+
+[ "$(uname -s)" == "Linux" ] && lsed() {
+    sed "$@"
+}
+
+[ "$(uname -s)" == "Darwin" ] && lsed() {
+    gsed "$@"
+}
+
+# update yaml
+k8s_manifests_file="release/kubernetes-manifests.yaml"
+
+image="$REPO_PREFIX/$SERVICE:$TAG"
+
+pattern="^(\s*)image:\s.*$SERVICE(.*)(\s*)"
+replace="\1image: $image\3"
+lsed -i -r "s|$pattern|$replace|g" "${k8s_manifests_file}"
+echo "Done"
+
+# create git release / push to new branch
+# git checkout -b "release/${TAG}"
+git add "."
+git commit --allow-empty -m "Release $TAG"
+log "Pushing k8s manifests to ${TAG}..."
+git tag "$TAG"
+git push --set-upstream origin master
+git push --tags
+
+log "Successfully tagged release $TAG."
--- src/main.go
+++ src/main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-    // "runtime"
 	"net/http"
-	"time"
-	"github.com/mackerelio/go-osstat/memory"
-	"github.com/mackerelio/go-osstat/cpu"
+	// "time"
+	// "github.com/mackerelio/go-osstat/memory"
+	// "github.com/mackerelio/go-osstat/cpu"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
 )
 
 func main() {
@@ -24,8 +24,9 @@ func main() {
 			osVersion = "unknown"
 		}
 		htmlCode := fmt.Sprintf("<h1>OS version is %s\n</h1>",osVersion)
-		dt := time. Now()
-		candidateInfo := fmt.Sprintf("<h1>Xendit - Trial - Gaurav Kumar - 12-04-2022 - %s</h1>",dt.Format("01-02-2006"))
+		// dt := time. Now()
+		// candidateInfo := fmt.Sprintf("<h1>Xendit - Trial - Gaurav Kumar - 12-04-2022 - %s</h1>",dt.Format("01-02-2006"))
+		// htmlCode = htmlCode+candidateInfo
 		// topCmd := "/usr/bin/top -n 1|head -2"
 		// topOut, err := exec.Command("sh","-c",topCmd).Output()
 		// cpuMemoryUsage := string(topOut)
@@ -34,40 +35,44 @@ func main() {
 		// 	fmt.Printf("%s", err)
 		// 	cpuMemoryUsage = "Error in fetching CPU/Memory usage"
 		// }
-		memory, err := memory.Get()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err)
-			return
-		}
+		// memory, err := memory.Get()
+		// if err != nil {
+		// 	fmt.Fprintf(os.Stderr, "%s\n", err)
+		// 	return
+		// }
 
-		memoryTotal := int(memory.Total/1024)
-		memoryUsed := int(memory.Used/1024)
-		memoryCached := int(memory.Cached/1024)
-		memoryFree := int(memory.Free/1024)
+		// memoryTotal := int(memory.Total/1024)
+		// memoryUsed := int(memory.Used/1024)
+		// memoryCached := int(memory.Cached/1024)
+		// memoryFree := int(memory.Free/1024)
 
-		memoryUsage := fmt.Sprintf("Memory Usage:</br>Total: %dK, Used: %dK, Cached: %dK, Free: %dk</br>", memoryTotal,memoryUsed,memoryCached,memoryFree)
+		// memoryUsage := fmt.Sprintf("Memory Usage:</br>Total: %dK, Used: %dK, Cached: %dK, Free: %dk</br>", memoryTotal,memoryUsed,memoryCached,memoryFree)
 
-		before, err := cpu.Get()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err)
-			return
-		}
-		time.Sleep(time.Duration(1) * time.Second)
-		after, err := cpu.Get()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err)
-			return
-		}
-		total := float64(after.Total - before.Total)
-		cpuUser := float64(after.User-before.User)/total*100
-		cpuSystem := float64(after.System-before.System)/total*100
-		cpuIdle := float64(after.Idle-before.Idle)/total*100
+		// before, err := cpu.Get()
+		// if err != nil {
+		// 	fmt.Fprintf(os.Stderr, "%s\n", err)
+		// 	return
+		// }
+		// time.Sleep(time.Duration(1) * time.Second)
+		// after, err := cpu.Get()
+		// if err != nil {
+		// 	fmt.Fprintf(os.Stderr, "%s\n", err)
+		// 	return
+		// }
+		// total := float64(after.Total - before.Total)
+		// cpuUser := float64(after.User-before.User)/total*100
+		// cpuSystem := float64(after.System-before.System)/total*100
+		// cpuIdle := float64(after.Idle-before.Idle)/total*100
 
-		cpuUsage := fmt.Sprintf("CPU Usage:</br>User: %f, System: %f, Idle: %f", cpuUser,cpuSystem,cpuIdle)
+		// cpuUsage := fmt.Sprintf("CPU Usage:</br>User: %f, System: %f, Idle: %f", cpuUser,cpuSystem,cpuIdle)
 
-		cpuMemoryUsageHtlm := fmt.Sprintf("<h1>Current CPU/Memory Usage:</br></h1><b>%s</b></br><b>%s</b>",memoryUsage,cpuUsage)
-		fmt.Fprintf(w, htmlCode+candidateInfo+cpuMemoryUsageHtlm)
+		// cpuMemoryUsageHtlm := fmt.Sprintf("<h1>Current CPU/Memory Usage:</br></h1><b>%s</b></br><b>%s</b>",memoryUsage,cpuUsage)	// htmlCode = htmlCode+cpuMemoryUsageHtlm
+
+		
+		fmt.Fprintf(w, htmlCode)
 	})
+	r.HandleFunc("/_healthz", func(w http.ResponseWriter, _ *http.Request) { fmt.Fprint(w, "ok") })
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
 
-	http.ListenAndServe(":80", r)
+	http.ListenAndServe(":80", loggedRouter)
 }
