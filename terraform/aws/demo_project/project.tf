808ef2b8ec1c4f8fc3521e5cc784492ae441f13c
Gaurav Kumar <gaurav.jgec@gmail.com>
2022-04-18 23:39:56 +0530

    Adding tf code

--- terraform/aws/demo_project/project.tf
+++ terraform/aws/demo_project/project.tf
@@ -1,6 +1,6 @@
 resource "buddy_project" "github" {
   domain              = "gauravjgec"
   display_name        = "demo"
-  integration_id      = "Nz7x3VXyMYerB5jKeOBQn6LlRG"
+  integration_id      = ""
   external_project_id = "gaurav-fk/test"
 }
